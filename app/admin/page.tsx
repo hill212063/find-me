98149dfda50995cfa5e0cb470f4693403a3c1086
inigo <inigo@core>
2025-02-02 15:25:01 +0700

    Add web socket for sending admin location to server

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,16 +1,57 @@
 "use client";
 
-import React, { useState } from 'react'
+import { socket } from '@/socket';
+import React, { useEffect, useRef, useState } from 'react'
 
 export default function SendLocPage() {
 
-  const [streamState, setStreamState] = useState(false);
+  const [isStream, setIsStream] = useState(false);
+  const intervalIdRef = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => { initSocket(); }, [])
+
+  const initSocket = async () => {
+    await fetch('/api/socket');
+    socket.on('connect', () => {
+      console.log('connected')
+    })
+    return null;
+  }
+
+  useEffect(() => {
+    console.log(isStream)
+    if (isStream) {
+      intervalIdRef.current = setInterval(() => {
+        streamingLocation();
+        // console.log(intervalIdRef.current)
+      }, 1000);
+    } else {
+      console.log("Stop" + intervalIdRef.current);
+      if (intervalIdRef.current) clearInterval(intervalIdRef.current);
+    }
+  }, [isStream]);
+
+  const onStream = () => {
+    setIsStream(!isStream);
+  }
+
+  const streamingLocation = () => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const msg = {
+        lat: pos.coords.latitude,
+        lon: pos.coords.longitude
+      }
+      // console.log("send location");
+      // console.log(msg);
+      socket.emit("send-location", msg);
+    })
+  }
 
   return (
     <div>
       <h1>Send location page</h1>
-      <button type='button' onClick={() => setStreamState(!streamState)}>
-        {!streamState ? "Stream location" : "Streaming location..."}
+      <button type='button' onClick={onStream}>
+        {isStream ? "Streaming location..." : "Stream location"}
       </button>
     </div>
   )
---
+++ pages/api/socket.ts
@@ -0,0 +1,36 @@
+import type { Server as HTTPServer } from 'http'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import type { Socket as NetSocket } from 'net'
+import { Server, type Server as IOServer } from 'socket.io'
+import { socket } from '@/socket'
+
+interface SocketServer extends HTTPServer {
+  io?: IOServer | undefined
+}
+
+interface SocketWithIO extends NetSocket {
+  server: SocketServer
+}
+
+interface NextApiResponseWithSocket extends NextApiResponse {
+  socket: SocketWithIO
+}
+
+const SocketHandler = (req: NextApiRequest, res: NextApiResponseWithSocket) => {
+  if (res.socket.server.io) {
+    console.log('Socket is already running')
+  } else {
+    console.log('Socket is initializing')
+    const io = new Server(res.socket.server)
+    res.socket.server.io = io
+
+    io.on('connection', socket => {
+      socket.on('send-location', msg => {
+        socket.broadcast.emit('update-location', msg)
+      })
+    })
+  }
+  res.end()
+}
+
+export default SocketHandler
