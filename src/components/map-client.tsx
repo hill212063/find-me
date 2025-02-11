7d65f8d5de3e9fbcd46f01963356c433d85f91c4
hill212063 <hill_sup@hotmail.com>
2025-02-11 22:37:09 +0700

    Refactor marker to marker wrapper and display 2 marker in map

--- src/components/map-client.tsx
+++ src/components/map-client.tsx
@@ -4,16 +4,17 @@ import { LatLngExpression } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer } from "react-leaflet";
-import MarkerAdmin from './marker-admin';
+import MarkerWrapper from './marker-wrapper';
 import { socket } from './socket';
 
 
 const MapClient: React.FC = () => {
 
   const bangkokLocation: LatLngExpression = [13.7563, 100.5018];
-  const [currentLocation, setCurrentLocation] = useState<LatLngExpression>(bangkokLocation);
+  const [adminLocation, setAdminLocation] = useState<LatLngExpression>(bangkokLocation);
+  const [clientLocation, setClientLocation] = useState<LatLngExpression>(bangkokLocation);
 
-  useEffect(() => { initSocket(); }, [])
+  useEffect(() => { initSocket(); watchClientLocation(); }, [])
 
   const initSocket = async () => {
     await fetch('/api/socket');
@@ -21,18 +22,27 @@ const MapClient: React.FC = () => {
       console.log('client connected')
     })
     socket.on('update-location', msg => {
-      setCurrentLocation(msg)
+      setAdminLocation(msg)
     })
   }
 
+  const watchClientLocation = () => {
+    navigator.geolocation.watchPosition((pos) => {
+      const clientLocation: LatLngExpression = [pos.coords.latitude, pos.coords.longitude];
+      setClientLocation(clientLocation);
+    })
+  }
+
+
   return (
     <div style={{ width: "100%", height: "100%", aspectRatio: 1 }}>
-      <MapContainer style={{ width: "100%", height: "100%" }} center={currentLocation} zoom={13} scrollWheelZoom={true}>
+      <MapContainer style={{ width: "100%", height: "100%" }} center={adminLocation} zoom={13} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <MarkerAdmin markerPosition={currentLocation} markerName='Current' />
+        <MarkerWrapper markerPosition={adminLocation} markerName='Admin' />
+        <MarkerWrapper markerPosition={clientLocation} markerName='Client' />
       </MapContainer>
     </div>
   )
