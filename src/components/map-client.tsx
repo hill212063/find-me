307909b2a25e367f09f90d0861e50ab5a10efe3a
hill212063 <hill_sup@hotmail.com>
2025-02-12 11:36:01 +0700

    Add routing from OSRM api and draw route

--- src/components/map-client.tsx
+++ src/components/map-client.tsx
@@ -1,20 +1,22 @@
 "use client";
 
-import { LatLngExpression } from 'leaflet';
+import { LatLngExpression, LatLngTuple } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Polyline, TileLayer } from "react-leaflet";
 import MarkerWrapper from './marker-wrapper';
 import { socket } from './socket';
 
-
 const MapClient: React.FC = () => {
 
-  const bangkokLocation: LatLngExpression = [13.7563, 100.5018];
-  const [adminLocation, setAdminLocation] = useState<LatLngExpression>(bangkokLocation);
-  const [clientLocation, setClientLocation] = useState<LatLngExpression>(bangkokLocation);
+  const bangkokLocation: LatLngTuple = [13.7563, 100.5018];
+  const [adminLocation, setAdminLocation] = useState<LatLngTuple>(bangkokLocation);
+  const [clientLocation, setClientLocation] = useState<LatLngTuple>(bangkokLocation);
+  const [route, setRoute] = useState<LatLngTuple[]>([]);
+  const OSRM_ROUTE_URL = `https://router.project-osrm.org/route/v1/driving/${clientLocation[1]},${clientLocation[0]};${adminLocation[1]},${adminLocation[0]}?overview=full&geometries=geojson`;
 
   useEffect(() => { initSocket(); watchClientLocation(); }, [])
+  useEffect(() => { getRoutingData(); }, [clientLocation, adminLocation])
 
   const initSocket = async () => {
     await fetch('/api/socket');
@@ -33,6 +35,21 @@ const MapClient: React.FC = () => {
     })
   }
 
+  const getRoutingData = () => {
+    fetch(OSRM_ROUTE_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        if (data.routes.length > 0) {
+          const coordinates: LatLngTuple[] = data.routes[0].geometry.coordinates.map(
+            (point: [number, number]) => [point[1], point[0]] // Swap lng, lat → lat, lng
+          );
+          setRoute(coordinates)
+        }
+      })
+      .catch((err) => console.error("Error fetching route:", err));
+  }
+
 
   return (
     <div style={{ width: "100%", height: "100%", aspectRatio: 1 }}>
@@ -41,8 +58,9 @@ const MapClient: React.FC = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <MarkerWrapper markerPosition={adminLocation} markerName='Admin' />
-        <MarkerWrapper markerPosition={clientLocation} markerName='Client' />
+        <MarkerWrapper markerPosition={adminLocation} markerName="Admin" />
+        <MarkerWrapper markerPosition={clientLocation} markerName="Client" />
+        {route.length > 0 && <Polyline positions={route} color="blue" />}
       </MapContainer>
     </div>
   )
